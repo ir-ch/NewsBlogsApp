89f24ad0f0862f1ff1f4d8bad94733da3ac6f356
ir-ch <ir-ch@gmail.com>
2025-10-02 13:42:45 +0100

    add functionality for Weather widget

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,25 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import './Weather.css'
 
 const Weather = () => {
+
+    const apiKey = import.meta.env.VITE_NEWS_APP_WEATHER_API_KEY
+
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState("")
+
+    useEffect(() => {
+        const fetchDefaultLocation = async () => {
+            const defaultLocation = "Tbilisi"
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&units=Metric&appid=${apiKey}`;
+
+            const response = await axios.get(url)
+            setData(response.data)
+        }
+        fetchDefaultLocation()
+    }, [])
+
+    const search = async () => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${apiKey}`;
+
+        try {
+            const response = await axios.get(url)
+            if (response.data.cod !== 200) {
+                setData({notFound: true})
+            } else {
+                setData(response.data)
+                setLocation('')
+            }
+        } catch (error) {
+            if (error.response && error.response.status === 404) {
+                setData({notFound: true})
+            } else {
+                console.error("An unexpected error occurred", error)
+            }
+        }
+    }
+
+    const handleInputChange = (e) => {
+        setLocation(e.target.value)
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            search()
+        }
+    }
+
+    const getWeatherIcon = (weatherType) => {
+        switch(weatherType) {
+            case "Clear":
+                return <i className='bx bxs-sun'></i>
+            case "Clouds":
+                return <i className='bx bxs-cloud'></i>
+            case "Rain":
+                return <i className='bx bxs-rain'></i>
+            case "Thumderstorm":
+                return <i className='bx bxs-lightning'></i>
+            case "Snow":
+                return <i className='bx bxs-snow'></i>
+            case "Haze":
+            case "Mist":
+                return <i className='bx bxs-cloud'></i>
+            default:
+                return <i className='bx bxs-cloud'></i>
+        }
+    }
+
     return (
         <div className='weather'>
             <div className="search">
                 <div className="search-top">
                     <i className="fa-solid fa-location-dot"></i>
-                    <div className="location">Tbilisi</div>
+                    <div className="location">{data.name}</div>
                 </div>
                 <div className="search-location">
-                    <input type="text" placeholder="Enter Location" />
-                    <i className="fa-solid fa-magnifying-glass"></i>
+                    <input type="text" placeholder="Enter Location" value={location} onChange={handleInputChange} onKeyDown={handleKeyDown} />
+                    <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
                 </div>
             </div>
-            <div className="weather-data">
-                <i className="bx bxs-sun"></i>
-                <div className="weather-type">Clear</div>
-                <div className="temp">25°</div>
-            </div>
+            {data.notFound ? (<div className='not-found'>Not Found 😒</div>) : (
+                <div className="weather-data">
+                    {data.weather && data.weather[0] && getWeatherIcon(data.weather[0].main)}
+                    <div className="weather-type">
+                        {data.weather ? data.weather[0].main : null}
+                    </div>
+                    <div className="temp">
+                        {data.main ? `${Math.floor(data.main.temp)}°` : null}
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
