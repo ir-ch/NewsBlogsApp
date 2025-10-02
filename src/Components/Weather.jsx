141baaeca272163bcbc5f9f3ddcb1e6ad30462b8
ir-ch <ir-ch@gmail.com>
2025-10-02 10:17:25 +0100

    create Weather widget and style it

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -4,7 +4,23 @@ import './Weather.css'
 
 const Weather = () => {
     return (
-        <div className='weather'>Weather</div>
+        <div className='weather'>
+            <div className="search">
+                <div className="search-top">
+                    <i className="fa-solid fa-location-dot"></i>
+                    <div className="location">Tbilisi</div>
+                </div>
+                <div className="search-location">
+                    <input type="text" placeholder="Enter Location" />
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+            <div className="weather-data">
+                <i className="bx bxs-sun"></i>
+                <div className="weather-type">Clear</div>
+                <div className="temp">25°</div>
+            </div>
+        </div>
     )
 }
 
